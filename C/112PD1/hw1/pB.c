4e03c22d9f677a1883a0d29b75b3abf756f54007
Hell HBBD <paulyu602@gmail.com>
2023-09-14 11:32:17 +0800

    finish pB.c in "112PD1/hw1" directory

---
+++ C/112PD1/hw1/pB.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int reverse(int n){
+	int tens = (n%100)/10;
+	int units = n%10;
+	return n/100*100 + tens + units*10;
+}
+
+int main(){
+	int N;
+	scanf("%d",&N);
+	printf("%d",2023*reverse(N));
+}
