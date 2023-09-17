7755af0916d96adb5bbe9bba003bbfebb6378dcc
Hell HBBD <paulyu602@gmail.com>
2023-09-17 16:03:28 +0800

    finish "zeroJudge/a004.c~a009.c"

---
+++ C/zeroJudge/a004.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int year;
+	while (scanf("%d",&year) != EOF){
+		if (year%400 == 0 || (year%4 == 0 && year%100 != 0))
+			puts("閏年");
+		else
+			puts("平年");
+	}
+	return 0;
+}
---
+++ C/zeroJudge/a009.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	char c;
+	while ((c = getchar()) != '\n'){
+		putchar(c-7);
+	}
+	return 0;
+}
