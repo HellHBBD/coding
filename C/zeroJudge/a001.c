2fcb00ca37799fcf7b834fcacc6b40d433b9ef34
Hell HBBD <paulyu602@gmail.com>
2023-09-17 14:05:25 +0800

    finish "zeroJudge/a001.c~a003.c"

--- C/zeroJudge/a001.c
+++ C/zeroJudge/a001.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int getString(char *string){
-	char c;
-	string = 0;
-	int length = 0;
-	while ((c = getchar()) != '\n'){
-		string = realloc(string,sizeof(char)*(length+1));
-		string[length++] = c;
-	}
-	string = realloc(string,sizeof(char)*(length+1));
-	string[length++] = '\0';
-}
 
 int main(){
-	char *string;
-	getString(string);
-	printf("hello, %s",string);
-	free(string);
+	char string[10];
+	scanf("%s",string);
+	printf("hello, %s\n",string);
 	return 0;
 }
