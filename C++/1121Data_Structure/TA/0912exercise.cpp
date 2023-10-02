415848c31588125509f965f7860c12f250200b9a
Hell HBBD <paulyu602@gmail.com>
2023-10-02 18:25:06 +0800

    arrange C++ directory

---
+++ C++/1121Data_Structure/TA/0912exercise.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int A[]={80,2,22,4,0,8,100,20,1,50};
+
+	for (int i = 1; i < 10; i++) { 
+		int j = i - 1; 
+		while(j >= 0){
+			if(A[j+1] < A[j]){
+				A[j+1] = A[j+1] ^ A[j];
+				A[j] = A[j+1] ^ A[j];
+				A[j+1] = A[j+1] ^ A[j];
+				j--;
+			}
+			else{
+				break; 
+			}
+
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		cout<< A[i] <<",";
+	}
+	cout<< "end";
+	return 0;
+}
---
+++ C++/1121Data_Structure/homework/hw1.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+template <class T> void InsertionSort(T arr[], int n) {}
+
+int main() {
+	// your code goes here
+	return 0;
+}
