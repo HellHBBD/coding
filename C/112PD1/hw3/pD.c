8650870ab6764e010fa2c5b18f3025a7ddedbef0
Hell HBBD <paulyu602@gmail.com>
2023-10-06 00:24:09 +0800

    add more judgement

--- C/112PD1/hw3/pD.c
+++ C/112PD1/hw3/pD.c
@@ -4,7 +4,9 @@
 int main(){
     long long a,b;
     scanf("%lld%lld",&a,&b);
-	if (a > LLONG_MAX-b || a < LLONG_MIN-b)
+	if ((a > 0 && b < 0) || (a < 0 && b > 0))
+		printf("No");
+	else if (a > LLONG_MAX-b || a < LLONG_MIN-b)
 		printf("Yes");
 	else
 		printf("No");
