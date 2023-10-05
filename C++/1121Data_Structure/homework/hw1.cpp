ce8aeb1e9ae33bf3a2a7a951f6896bdb73b0217d
Hell HBBD <paulyu602@gmail.com>
2023-10-05 21:34:51 +0800

    finish all function

--- C++/1121Data_Structure/homework/hw1.cpp
+++ C++/1121Data_Structure/homework/hw1.cpp
@@ -1,7 +1,52 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
-using namespace std;
+
+template <class T>
+void swap(T &,T &);
+
+void shuffle(std::string [][13]);
+
+void printDeck(std::string [][13]);
+
+void printHand(std::string,std::string []);
+
+void deal(std::string [][13],std::string [],std::string [],std::string [],std::string []);
+
+int compare(const std::string &,const std::string &);
+
+int compare(const int &,const int &);
+
+template <class T>
+void InsertionSort(T [],int);
+
+int calculateValue(std::string []);
+
+int main() {
+	std::string poker[4][13] = { //use "std::string" to store each card is more efficient
+		"SA","S2","S3","S4","S5","S6","S7","S8","S9","ST","SJ","SQ","SK",
+		"HA","H2","H3","H4","H5","H6","H7","H8","H9","HT","HJ","HQ","HK",
+		"DA","D2","D3","D4","D5","D6","D7","D8","D9","DT","DJ","DQ","DK",
+		"CA","C2","C3","C4","C5","C6","C7","C8","C9","CT","CJ","CQ","CK"
+	};
+	std::string East[13],Sorth[13],West[13],North[13];
+	shuffle(poker);
+	deal(poker,North,East,Sorth,West);
+	printDeck(poker);
+	InsertionSort(North,13);
+	printHand("North",North);
+	InsertionSort(East,13);
+	printHand("East",East);
+	InsertionSort(Sorth,13);
+	printHand("Sorth",Sorth);
+	InsertionSort(West,13);
+	printHand("West",West);
+	std::cout << "North's MAX_MIN_Sum Value: " << calculateValue(North) << std::endl;
+	std::cout << "East's MAX_MIN_Sum Value: " << calculateValue(East) << std::endl;
+	std::cout << "Sorth's MAX_MIN_Sum Value: " << calculateValue(Sorth) << std::endl;
+	std::cout << "West's MAX_MIN_Sum Value: " << calculateValue(West) << std::endl;
+	return 0;
+}
 
 template <class T>
 void swap(T &a,T &b){ //swap using reference
@@ -10,7 +55,7 @@ void swap(T &a,T &b){ //swap using reference
 	b = temp;
 }
 
-void shuffle(string poker[4][13]){
+void shuffle(std::string poker[4][13]){
 	srand(time(0));
 	int switchTime = rand(); //shuffle random times
 	while (switchTime--){ //while (0) stop
@@ -20,38 +65,38 @@ void shuffle(string poker[4][13]){
 	}
 }
 
-void printDeck(string poker[4][13]){
+void printDeck(std::string poker[4][13]){
 	for (int i = 0;i < 4;i++){
 		for (int j = 0;j < 13;j++){
-			cout << poker[i][j] << " | ";
+			std::cout << poker[i][j] << " | ";
 		}
-		cout << endl;
-		cout << "-----------------------------------------------------------------" << endl << endl;
+		std::cout << std::endl;
+		std::cout << "-----------------------------------------------------------------" << std::endl << std::endl;
 	}
 }
 
-void printHand(string playerName,string player[13]){
-	cout << playerName << endl;
-	cout << "S: ";
+void printHand(std::string playerName,std::string player[13]){
+	std::cout << playerName << std::endl;
+	std::cout << "S: ";
 	for (int i = 0;i < 13;i++)
 		if (player[i][0] == 'S')
-			cout << player[i][1] << " ";
-	cout << endl << "H: ";
+			std::cout << player[i][1] << " ";
+	std::cout << std::endl << "H: ";
 	for (int i = 0;i < 13;i++)
 		if (player[i][0] == 'H')
-			cout << player[i][1] << " ";
-	cout << endl << "D: ";
+			std::cout << player[i][1] << " ";
+	std::cout << std::endl << "D: ";
 	for (int i = 0;i < 13;i++)
 		if (player[i][0] == 'D')
-			cout << player[i][1] << " ";
-	cout << endl << "C: ";
+			std::cout << player[i][1] << " ";
+	std::cout << std::endl << "C: ";
 	for (int i = 0;i < 13;i++)
 		if (player[i][0] == 'C')
-			cout << player[i][1] << " ";
-	cout << endl << endl;
+			std::cout << player[i][1] << " ";
+	std::cout << std::endl << std::endl;
 }
 
-void deal(string poker[4][13],string player1[13],string player2[13],string player3[13],string player4[13]){
+void deal(std::string poker[4][13],std::string player1[13],std::string player2[13],std::string player3[13],std::string player4[13]){
 	for (int i = 0;i < 12;i++){
 		player1[i] = poker[i%4][i/4*4];
 		player2[i] = poker[i%4][i/4*4+1];
@@ -64,7 +109,7 @@ void deal(string poker[4][13],string player1[13],string player2[13],string playe
 	player4[12] = poker[3][12];
 }
 
-int compare(const string &card1,const string &card2){
+int compare(const std::string &card1,const std::string &card2){
 	int n1 = 0,n2 = 0;
 	switch (card1[0]){
 		case 'S': n1 += 13;
@@ -111,6 +156,10 @@ int compare(const string &card1,const string &card2){
 	return n1-n2;
 }
 
+int compare(const int &a,const int &b){
+	return a-b;
+}
+
 template <class T>
 void InsertionSort(T arr[], int n){
 	for (int unsortIndex = 1;unsortIndex < n;unsortIndex++){
@@ -123,24 +172,51 @@ void InsertionSort(T arr[], int n){
 	}
 }
 
-int main() {
-	string poker[4][13] = { //use "string" to store each card is more efficient
-		"SA","S2","S3","S4","S5","S6","S7","S8","S9","ST","SJ","SQ","SK",
-		"HA","H2","H3","H4","H5","H6","H7","H8","H9","HT","HJ","HQ","HK",
-		"DA","D2","D3","D4","D5","D6","D7","D8","D9","DT","DJ","DQ","DK",
-		"CA","C2","C3","C4","C5","C6","C7","C8","C9","CT","CJ","CQ","CK"
-	};
-	string East[13],Sorth[13],West[13],North[13];
-	shuffle(poker);
-	deal(poker,North,East,Sorth,West);
-	printDeck(poker);
-	InsertionSort(North,13);
-	printHand("North",North);
-	InsertionSort(East,13);
-	printHand("East",East);
-	InsertionSort(Sorth,13);
-	printHand("Sorth",Sorth);
-	InsertionSort(West,13);
-	printHand("West",West);
-	return 0;
+int calculateValue(std::string player[13]){
+	int value[13];
+	for (int i = 0;i < 13;i++){
+		switch (player[i][1]){
+			case 'K':
+				value[i] = 13;
+				break;
+			case 'Q':
+				value[i] = 12;
+				break;
+			case 'J':
+				value[i] = 11;
+				break;
+			case 'T':
+				value[i] = 10;
+				break;
+			case '9':
+				value[i] = 9;
+				break;
+			case '8':
+				value[i] = 8;
+				break;
+			case '7':
+				value[i] = 7;
+				break;
+			case '6':
+				value[i] = 6;
+				break;
+			case '5':
+				value[i] = 5;
+				break;
+			case '4':
+				value[i] = 4;
+				break;
+			case '3':
+				value[i] = 3;
+				break;
+			case '2':
+				value[i] = 2;
+				break;
+			case 'A':
+				value[i] = 1;
+				break;
+		}
+	}
+	InsertionSort(value,13);
+	return value[0]+value[1]+value[11]+value[12];
 }
