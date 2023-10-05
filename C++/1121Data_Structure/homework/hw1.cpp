abcad2ce8894ddff0d3e28cc48df4c89c60cc29a
Hell HBBD <paulyu602@gmail.com>
2023-10-05 16:40:18 +0800

    finish deal and insertion sort function in "hw1.cpp/1121Data_Structure"

--- C++/1121Data_Structure/homework/hw1.cpp
+++ C++/1121Data_Structure/homework/hw1.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 template <class T>
 void swap(T &a,T &b){ //swap using reference
-					  //reuse codes when shuffle or sort
 	T temp = a;
 	a = b;
 	b = temp;
@@ -31,89 +30,76 @@ void print(string poker[4][13]){
 	}
 }
 
+void deal(string poker[4][13],string player1[13],string player2[13],string player3[13],string player4[13]){
+	for (int i = 0;i < 12;i++){
+		player1[i] = poker[i%4][i/4*4];
+		player2[i] = poker[i%4][i/4*4+1];
+		player3[i] = poker[i%4][i/4*4+2];
+		player4[i] = poker[i%4][i/4*4+3];
+	}
+	player1[12] = poker[0][12];
+	player2[12] = poker[1][12];
+	player3[12] = poker[2][12];
+	player4[12] = poker[3][12];
+}
+
 int compare(const string &card1,const string &card2){
 	int n1 = 0,n2 = 0;
 	switch (card1[0]){
-		case 'S':
-			n1 += 13;
-		case 'H':
-			n1 += 13;
-		case 'D':
-			n1 += 13;
-		case 'C':
-			;
+		case 'S': n1 += 13;
+		case 'H': n1 += 13;
+		case 'D': n1 += 13;
+		case 'C': ;
 	}
 	switch (card2[0]){
-		case 'S':
-			n2 += 13;
-		case 'H':
-			n2 += 13;
-		case 'D':
-			n2 += 13;
-		case 'C':
-			;
+		case 'S': n2 += 13;
+		case 'H': n2 += 13;
+		case 'D': n2 += 13;
+		case 'C': ;
 	}
 	switch (card1[1]){
-		case 'K':
-			n1++;
-		case 'Q':
-			n1++;
-		case 'J':
-			n1++;
-		case 'T':
-			n1++;
-		case '9':
-			n1++;
-		case '8':
-			n1++;
-		case '7':
-			n1++;
-		case '6':
-			n1++;
-		case '5':
-			n1++;
-		case '4':
-			n1++;
-		case '3':
-			n1++;
-		case '2':
-			n1++;
-		case '1':
-			;
+		case 'K': n1++;
+		case 'Q': n1++;
+		case 'J': n1++;
+		case 'T': n1++;
+		case '9': n1++;
+		case '8': n1++;
+		case '7': n1++;
+		case '6': n1++;
+		case '5': n1++;
+		case '4': n1++;
+		case '3': n1++;
+		case '2': n1++;
+		case '1': ;
 	}
 	switch (card2[1]){
-		case 'K':
-			n2++;
-		case 'Q':
-			n2++;
-		case 'J':
-			n2++;
-		case 'T':
-			n2++;
-		case '9':
-			n2++;
-		case '8':
-			n2++;
-		case '7':
-			n2++;
-		case '6':
-			n2++;
-		case '5':
-			n2++;
-		case '4':
-			n2++;
-		case '3':
-			n2++;
-		case '2':
-			n2++;
-		case '1':
-			;
+		case 'K': n2++;
+		case 'Q': n2++;
+		case 'J': n2++;
+		case 'T': n2++;
+		case '9': n2++;
+		case '8': n2++;
+		case '7': n2++;
+		case '6': n2++;
+		case '5': n2++;
+		case '4': n2++;
+		case '3': n2++;
+		case '2': n2++;
+		case '1': ;
 	}
 	return n1-n2;
 }
 
 template <class T>
 void InsertionSort(T arr[], int n){
+	for (int unsortIndex = 1;unsortIndex < n;unsortIndex++){
+		for (int currentIndex = unsortIndex;currentIndex > 0;currentIndex--){
+			if (compare(arr[currentIndex],arr[currentIndex-1]) < 0)
+				break;
+			else
+				swap(arr[currentIndex],arr[currentIndex-1]);
+		}
+	}
 }
 
 int main() {
@@ -123,16 +109,13 @@ int main() {
 		"DA","D2","D3","D4","D5","D6","D7","D8","D9","DT","DJ","DQ","DK",
 		"CA","C2","C3","C4","C5","C6","C7","C8","C9","CT","CJ","CQ","CK"
 	};
-	for (int i = 0;i < 52;i++){
-		for (int j = 0;j < 52;j++){
-			string card1 = poker[i/13][i%13];
-			string card2 = poker[j/13][j%13];
-			cout << card1 << " " << card2 << " " << compare(card1,card2) << endl;
-		}
-		puts("");
-	}
-	//print(poker);
-	//shuffle(poker);
-	//print(poker);
+	string East[13],Sorth[13],West[13],North[13];
+	shuffle(poker);
+	deal(poker,North,East,Sorth,West);
+	print(poker);
+	InsertionSort(North,13);
+	InsertionSort(East,13);
+	InsertionSort(Sorth,13);
+	InsertionSort(West,13);
 	return 0;
 }
