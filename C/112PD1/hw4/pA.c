4fbb9f58415022e946166a83c70305692b41bb6b
HellHBBD <paulyu602@gmail.com>
2023-10-13 13:26:15 +0800

    fix format issue

--- C/112PD1/hw4/pA.c
+++ C/112PD1/hw4/pA.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int main(){
-    int days,starting_day;
-    printf("Enter number of days in month: ");
-    scanf("%d",&days);
-    printf("Enter starting day of the week (1=Sun, 7=Sat): ");
-    scanf("%d",&starting_day);
-    
+	int days,starting_day;
+	printf("Enter number of days in month: ");
+	scanf("%d",&days);
+	printf("Enter starting day of the week (1=Sun, 7=Sat): ");
+	scanf("%d",&starting_day);
 	int count = 0;
-	for (int i = -starting_day+1;i < days;i++,count++){
-		if (count%7 == 0)
-			puts("");
-		if (i < 0){
+	for (int i = 0;i < days;){
+		if (starting_day-1){
 			printf("   ");
+			starting_day--;
+			count++;
 			continue;
 		}
 		printf("%2d ",i+1);
+		i++;
+		count++;
+		if (count%7 == 0)
+			puts("");
 	}
-    return 0;
+	return 0;
 }
