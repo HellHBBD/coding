37ee58c8ee31f509931326ccfdf009c30da7d706
Hell HBBD <paulyu602@gmail.com>
2023-10-17 16:17:38 +0800

    finish index()

--- other/C/double_linked_list.c
+++ other/C/double_linked_list.c
@@ -26,7 +26,7 @@ void clear();
 
 struct List *find();
 
-struct Node *index();
+struct Node *index(struct List *,int);
 
 void reverse();
 
@@ -36,26 +36,32 @@ void print(struct List *);
 
 int main(){
 	struct List *list = malloc(sizeof(struct List));
+	list->length = 0;
 	list->head = 0;
 	list->tail = 0;
 	append(list,2);
 	append(list,3);
 	append(list,5);
-	print(list);
+	struct Node *test = index(list,0);
+	printf("%d\n",test->value);
+	test = index(list,1);
+	printf("%d\n",test->value);
+	test = index(list,2);
+	printf("%d\n",test->value);
 	free(list);
 	return 0;
 }
 
 void append(struct List *list,T value){
 	struct Node *new_node = malloc(sizeof(struct Node));
-	if (list->head && list->tail){
+	if (list->length){
 		new_node->previous = list->tail; //backward
 		list->tail = new_node;
 		new_node->previous->next = new_node; //forward
 		new_node->next = 0;
 		new_node->value = value; //assign
 	}
-	else{
+	else{ //empty
 		list->head = list->tail = new_node;
 		new_node->next = new_node->previous = 0;
 		new_node->value = value;
@@ -64,6 +70,21 @@ void append(struct List *list,T value){
 }
 
 void insert(struct List *list,int index,T value){
+}
+
+struct Node *index(struct List *list,int index){
+	struct Node *result = 0;
+	if (2*index <= list->length-1){
+		result = list->head;
+		for (int i = 0;i != index;i++)
+			result = result->next;
+	}
+	else{
+		result = list->tail;
+		for (int i = list->length-1;i != index;i--)
+			result = result->previous;
+	}
+	return result;
 }
 
 void print(struct List *list){
