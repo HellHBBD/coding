b371f7a9a99f35fc9bc2c48370bfe6480dcd4cea
HellHBBD <paulyu602@gmail.com>
2023-10-28 22:12:29 +0800

    finish option 2 not random version

--- C/112PD1/hw6/hw6.c
+++ C/112PD1/hw6/hw6.c
@@ -14,16 +14,39 @@ void Homework6_1(){
 		printf(" %d",array[i]);
 }
 
+void Homework6_2(){
+	int N;
+	scanf("%d",&N);
+	int array[N-1];
+	for (int i = 0;i < N;i++)
+		scanf("%d",&array[i]);
+	bool exist[N];
+	for (int i = 0;i < N-1;i++)
+		exist[array[i]-1] = true;
+	for (int i = 0;i < N;i++){
+		if (!exist[i]){
+			printf("%d",i+1);
+			break;
+		}
+	}
+}
+
+void Homework6_3(){
+}
+
+void Homework6_4(){
+}
+
 int main(){
 	int option;
 	scanf("%d",&option);
 	if (option == 1)
 		Homework6_1();
-	/* else if (option == 2) */
-	/* 	Homework6_2(); */
-	/* else if (option == 3) */
-	/* 	Homework6_3(); */
-	/* else if (option == 4) */
-	/* 	Homework6_4(); */
+	else if (option == 2)
+		Homework6_2();
+	else if (option == 3)
+		Homework6_3();
+	else if (option == 4)
+		Homework6_4();
 	return 0;
 }
