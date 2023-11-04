b2f26791c58766ec98fdac63747827ad43b379d6
HellHBBD <paulyu602@gmail.com>
2023-11-04 12:26:18 +0800

    add comments

--- other/C/dynamic_program_demo.c
+++ other/C/dynamic_program_demo.c
@@ -6,14 +6,13 @@ int *table;
 unsigned long long solved;
 
 int f(int n){
-	if (solved & (1 << n))
+	if (solved & (1 << n)) //test if n bit is 1
 		return table[n];
-	int size = log2(solved);
-	if (n > size){
+	if (n > log2(solved)){ //determine if n is bigger than max size
 		table = realloc(table,sizeof(int)*(n+1));
 	}
-	solved |= (1 << n);
-	table[n] = f(n-1) + f(n-2);
+	solved |= (1 << n); //set n bit as 1
+	table[n] = f(n-1) + f(n-2); //recursive and store the result
 	return table[n];
 }
 
@@ -23,9 +22,12 @@ int main(){
 	table[0] = 0;
 	table[1] = 1;
 	solved = 3;
+
+	//test
 	for (int i = 0;i < 20;i++)
 		printf("%d ",f(i));
 	puts("");
+
 	free(table);
 	return 0;
 }
