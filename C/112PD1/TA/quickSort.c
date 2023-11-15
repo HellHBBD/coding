d46e8d49574662b148e6f46548ed5761a4d1792b
Hell HBBD <paulyu602@gmail.com>
2023-11-16 00:28:40 +0800

    finish quickSort.c

--- C/112PD1/TA/quickSort.c
+++ C/112PD1/TA/quickSort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define swap(a, b) ((a) ^= (b) ^= (a) ^= (b))
 #define SIZE 10
 
@@ -8,31 +10,44 @@
 /* 	*b = *a; */
 /* } */
 
-void QSORT(int *array, int left, int right) {
+void print(int *array) {
+	for (int i = 0; i < SIZE; i++)
+		printf("%d ", array[i]);
+	puts("");
+}
 
-	if (left >= right)
-		return;
+void quickSort(int array[], int leftest, int rightest) {
+	if (leftest < rightest) {
+		int key = array[(leftest + rightest) / 2];
+		int left_index = leftest - 1;
+		int right_index = rightest + 1;
 
-	int l = left + 1;
-	int r = right;
-	int key = array[left];
-
-	///to-do///
-
-	QSORT(array);
-	QSORT(array);
+		while (1) {
+			while (array[++left_index] < key)
+				;
+			/* find left */
+			while (array[--right_index] > key)
+				;
+			/* find right */
+			if (left_index >= right_index)
+				break;
+			swap(array[left_index], array[right_index]);
+		}
+		quickSort(array, leftest, left_index - 1);	 //recursive at left
+		quickSort(array, right_index + 1, rightest); //recursive at right
+	}
 }
 
 int main(void) {
-
+	srand(time(0));
 	int array[SIZE];
-
 	for (int i = 0; i < SIZE; i++)
-		scanf("%d", &array[i]);
+		array[i] = rand() % 10;
 
-	QSORT(array, 0, SIZE - 1);
+	quickSort(array, 0, SIZE - 1);
 
 	for (int i = 0; i < SIZE; i++)
 		printf("%d ", array[i]);
+	puts("");
 	return 0;
 }
