8e899c5297188f7cb0da736cb1b364375d80f75b
Hell HBBD <paulyu602@gmail.com>
2023-11-23 16:54:50 +0800

    add hw8/

---
+++ C/112PD1/hw8/main2.c
@@ -0,0 +1,31 @@
+#include "hw8-2.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	static int mp[131][131][131];
+
+	int q = 1000;
+	while (q--) {
+		int opt;
+		scanf("%d", &opt);
+		if (opt == 1) {
+			char str[4] = {};
+			int val;
+
+			scanf("%s %d", str, &val);
+
+			int *pos = &(mp[str[0]][str[1]][str[2]]);
+			modify(pos, val);
+		} else {
+			char a[4], b[4];
+			scanf("%s %s", a, b);
+
+			int check = query(&mp, a, b);
+
+			printf((check == 1 ? "YES\n" : "NO\n"));
+		}
+	}
+
+	return 0;
+}
