88252168ac3c0d64d85b32ff53e43a3e70e64610
HellHBBD <paulyu602@gmail.com>
2023-11-26 15:55:03 +0800

    finish hw8

--- C/112PD1/hw8/hw8-2.c
+++ C/112PD1/hw8/hw8-2.c
@@ -1,17 +1,9 @@
 void modify(int *a, int val) {
-	*a = val;
+	*a = val + 1;
 }
 
 int query(int (*mp)[131][131][131], char a[4], char b[4]) {
 	int avalue = (*mp)[a[0]][a[1]][a[2]], bvalue = (*mp)[b[0]][b[1]][b[2]];
-	int check = 0;
-	if (avalue == 0)
-		check++;
-	else
-		check += avalue;
-	if (bvalue == 0)
-		check++;
-	else
-		check += bvalue;
+	int check = avalue+bvalue;
 	return (check % 2 == 0) ? 0 : 1;
 }
