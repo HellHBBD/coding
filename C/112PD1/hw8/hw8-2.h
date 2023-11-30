b92cf3beeeb1bfce14e939a2420b77f4033d6b64
Hell HBBD <paulyu602@gmail.com>
2023-11-30 11:29:54 +0800

    commit hw8-2.h

--- C/112PD1/hw8/hw8-2.h
+++ C/112PD1/hw8/hw8-2.h
@@ -2,6 +2,7 @@
 #define HW8-2_H
 
 void modify(int *a, int val);
-int query(int (*mp)[131][131][131], char a[4], char b[4]);
+int query(int ****mp, char a[4], char b[4]);
+// int query(int (*mp)[131][131][131], char a[4], char b[4]);
 
 #endif
