b0269fd41b5dd206462ca4a2b2d45a1647960ca0
Hell HBBD <paulyu602@gmail.com>
2023-11-30 23:01:49 +0800

    add hw9/

---
+++ C/112PD1/hw9/hw9.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+
+union Number {
+		float float_number;
+		unsigned float_bits;
+		double double_number;
+		unsigned long long double_bits;
+};
+
+void getNumber(union Number *number, char *format) {
+	if (strcmp(format, "fn") == 0) {
+		scanf("%f", &number->float_number);
+	} else if (strcmp(format, "fb") == 0) {
+		for (int i = sizeof(float) * 8; i >= 0; i--) {
+			char c = getchar();
+			if (c == '0')
+				number->float_bits |= (1 << i);
+			else if (c == '1')
+				number->float_bits &= ~(1 << i);
+		}
+		getchar();
+	} else if (strcmp(format, "dn") == 0) {
+		scanf("%lf", &number->double_number);
+	} else if (strcmp(format, "db") == 0) {
+		for (int i = sizeof(double) * 8; i >= 0; i--) {
+			char c = getchar();
+			if (c == '0')
+				number->double_bits |= (1 << i);
+			else if (c == '1')
+				number->double_bits &= ~(1 << i);
+		}
+		getchar();
+	}
+}
+
+void printNumber(union Number number, char *format) {
+	if (strcmp(format, "fn") == 0) {
+		printf("%f\n", number.float_number);
+	} else if (strcmp(format, "fb") == 0) {
+		for (int i = sizeof(float) * 8; i >= 0; i--)
+			if (number.float_bits & (1 << i))
+				putchar('1');
+			else
+				putchar('0');
+		puts("");
+	} else if (strcmp(format, "dn") == 0) {
+		printf("%e\n", number.double_number);
+	} else if (strcmp(format, "db") == 0) {
+		for (int i = sizeof(double) * 8; i >= 0; i--)
+			if (number.double_bits & (1 << i))
+				putchar('1');
+			else
+				putchar('0');
+		puts("");
+	}
+}
+
+int main() {
+	union Number f1, f2;
+	/* 1 */
+	f1.double_number = 1.175494350822287507968736537222245677818665556772087521508751706278417259454727172851560500000000000000000000000000000000e-38f;
+	printNumber(f1, "dn");
+	/* 2 */
+	f2.float_number = 0;
+	printNumber(f2, "fb");
+	f2.double_number = 0;
+	printNumber(f2, "db");
+	/* 3 */
+	f2.double_number = 1.175494350822287500e-38f;
+	if (f1.double_number == f2.double_number)
+		printf("  %.100e\n= %.100e\n", f1.double_number, f2.double_number);
+	else
+		printf("%.100e\n!= %.100e\n", f1.double_number, f2.double_number);
+	return 0;
+}
