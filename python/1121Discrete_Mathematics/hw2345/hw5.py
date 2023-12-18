ba847fce9b374c0c2b8b7ac8860fa6cdc03f13db
Hell HBBD <paulyu602@gmail.com>
2023-12-18 11:49:12 +0800

    finish part of hw5.py

--- python/1121Discrete_Mathematics/hw2345/hw5.py
+++ python/1121Discrete_Mathematics/hw2345/hw5.py
@@ -36,3 +36,26 @@ with open(filename,'r',encoding='utf8') as file:
                     nodes[start][end].append(weight)
 
 print(f'the network name is {title} with n={node} nodes and m={arc} arcs')
+d = [-1] * node
+pred = [-1] * node
+queue = []
+
+while 1:
+    s = eval(input('input the origin, if the origin is \'0\', then the code STOP:'))
+    if s == 0:
+        break
+    queue.append(s)
+    d[s-1] = 0
+    while queue != []:
+        tail = queue.pop(0)
+        for head,length in nodes[tail].items():
+            queue.append(head)
+            distance = d[tail-1] + min(length)
+            if d[head-1] == -1 or distance < d[head-1]:
+                d[head-1] = distance
+                pred[head-1] = tail
+    print(f'd:{d}')
+    print(f'p:{pred}')
+'''
+{1: {2: [7], 3: [5], 4: [1]}, 2: {5: [4]}, 3: {2: [7], 5: [9], 6: [9]}, 4: {2: [5], 7: [16]}, 5: {4: [4], 8: [1]}, 6: {7: [1]}, 7: {1: [2], 8: [4]}, 8: {6: [2], 7: [4]}}
+'''
