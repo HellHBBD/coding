013e11b676c91bd6aa18a56d5b5ce128b9879d6a
HellHBBD <paulyu602@gmail.com>
2023-12-24 21:01:38 +0800

    fixed endianness issue

--- C/112PD1/hw11/hw11.c
+++ C/112PD1/hw11/hw11.c
@@ -22,9 +22,8 @@ void input() {
 	struct prefix *current = 0;
 	while (1) {
 		memset(temp, 0, 5);
-		int scanCount = fscanf(init, "%hhu.%hhu.%hhu.%hhu/%hhu", temp, temp + 1, temp + 2, temp + 3, temp + 4);
+		int scanCount = fscanf(init, "%hhu.%hhu.%hhu.%hhu/%hhu", temp+3, temp + 2, temp + 1, temp , temp + 4);
 		if (scanCount == 4) {
-			/* temp[4] = (temp[0] ? 8 : 0) + (temp[1] ? 8 : 0) + (temp[2] ? 8 : 0) + (temp[3] ? 8 : 0); */
 			temp[4] = temp[3] ? 32 : temp[2] ? 24
 								   : temp[1] ? 16
 								   : temp[0] ? 8
@@ -63,7 +62,6 @@ void length_distribution(int i) {
 void segment(int d) {
 	int size = (1 << d);
 	group = malloc(sizeof(struct prefix *) * size + 1);
-	/* memset(group, 0, sizeof(struct prefix *) * size+1); */
 	for (int i = 0; i < size; i++) {
 		group[i] = malloc(sizeof(struct prefix));
 		group[i]->ip = (i << (32 - d));
@@ -93,12 +91,6 @@ void segment(int d) {
 		currentPrefix->next = current;
 		current = head;
 	}
-
-	/* i = 0; */
-	/* while (group[i++] != 0) */
-	/* 	; */
-	/* group[i] = group[size - 1]; */
-	/* group[size - 1] = 0; */
 }
 
 void prefix_insert() {
@@ -139,7 +131,7 @@ void print() {
 		int count = 0;
 		for (struct prefix *printNode = group[i]->next; printNode; printNode = printNode->next)
 			count++;
-		printf("The number of prefixes in group %d = %d.\n", i, count);
+		printf("The number of prefixes in group %d = %d\n", i, count);
 	}
 }
 
@@ -162,23 +154,23 @@ void clear() {
 unsigned long long begin, end;
 
 int main(int arvc, char **argv) {
-	/* input_file = argv[1]; */
-	/* insert_file = argv[2]; */
-	/* delete_file = argv[3]; */
-	/* search_file = argv[4]; */
-	/* d = atoi(argv[5]); */
-	input_file = "routing_table.txt";
-	insert_file = "inserted_prefixes.txt";
-	delete_file = "deleted_prefixes.txt";
-	search_file = "trace_file.txt";
-	d = 8;
+	input_file = argv[1];
+	insert_file = argv[2];
+	delete_file = argv[3];
+	search_file = argv[4];
+	d = atoi(argv[5]);
+	/* input_file = "data/routing_table.txt"; */
+	/* insert_file = "data/inserted_prefixes.txt"; */
+	/* delete_file = "data/deleted_prefixes.txt"; */
+	/* search_file = "data/trace_file.txt"; */
+	/* d = 8; */
 	/* begin = rdtsc(); */
 	input();
 	/* end = rdtsc(); */
 	/* printf("Execute cycles %llu \n", (end - begin)); */
 
-	for (int i = 0; i <= 32; i++)
-		length_distribution(i);
+	/* for (int i = 0; i <= 32; i++) */
+	/* 	length_distribution(i); */
 
 	segment(d);
 	print();
