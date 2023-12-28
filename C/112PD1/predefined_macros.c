3d6a02ed8a7fc4ba75458414b883b385c815f23c
Hell HBBD <paulyu602@gmail.com>
2023-12-29 01:09:26 +0800

    add predefined_macros.c

---
+++ C/112PD1/predefined_macros.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	printf("Date: %s\n", __DATE__);
+	printf("Time: %s\n", __TIME__);
+	printf("Line: %d\n", __LINE__);
+	printf("Function: %s\n", __func__);
+	printf("File: %s\n", __FILE__);
+	/* CHECK_ZERO(4); */
+	printf("is standard: %d\n", __STDC__);
+	/* printf("is hosted implementation: %d\n",__STDC__HOSTED__); */
+	/* printf("Standard version: %s\n",__STDC__VERSION__); */
+#define TEST(condition, ...) ((condition) ? printf("Pass test: %s\n", #condition) : printf(__VA_ARGS__))
+	TEST(3 < 4, "not pass at line %d\n", __LINE__);
+	TEST(3 > 4, "not pass at line %d\n", __LINE__);
+#if defined(WIN32)
+	puts("System: Windows");
+#elif defined(LINUX)
+	puts("System: Linux");
+#endif
+/* #if 3<4 */
+/* #error wtf */
+#line 10 "bar.c"
+	printf("Line: %d\n", __LINE__);
+	printf("File: %s\n", __FILE__);
+	/* #pragma data(heap_size = > 1000, stack_size = > 2000) */
+	return 0;
+}
