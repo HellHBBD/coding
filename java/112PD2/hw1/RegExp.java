867decdc872ec0815efd9799614f1e2cee352439
Hell HBBD <paulyu602@gmail.com>
2024-03-03 19:09:38 +0800

    add comments and fixed implicit bugs

--- java/112PD2/hw1/RegExp.java
+++ java/112PD2/hw1/RegExp.java
@@ -12,13 +12,13 @@ class Regex {
 
 	public void palindrome()
 	{
-		for (int head = 0, tail = string.length() - 1; head <= tail; head++, tail--) {
-			if (string.charAt(head) != string.charAt(tail)) {
+		for (int head = 0, tail = string.length() - 1; head < tail; head++, tail--) {
+			if (string.charAt(head) != string.charAt(tail)) { //if once not match
 				System.out.print('N');
 				return;
 			}
 		}
-		System.out.print('Y');
+		System.out.print('Y'); //all match
 	}
 
 	public void contain(String str1)
@@ -26,17 +26,17 @@ class Regex {
 		int startIndex = 0;
 		int index = 0;
 		while (true) {
-			if (index == str1.length()) {
+			if (index == str1.length()) { //match whole str1
 				System.out.print('Y');
 				return;
-			}
-			else if (startIndex + index == string.length()){
+			} else if (startIndex + index == string.length()) {
+				//no match in whole string
 				System.out.print('N');
 				return;
-			}
-			if (string.charAt(startIndex + index) == str1.charAt(index)) {
+			} else if (string.charAt(startIndex + index) == str1.charAt(index)) {
+				//match one char -> test next char
 				index++;
-			} else {
+			} else { //not match -> test next substring
 				startIndex++;
 				index = 0;
 			}
@@ -48,22 +48,21 @@ class Regex {
 		int startIndex = 0;
 		int index = 0;
 		while (true) {
-			if (index == str2.length()) {
+			if (index == str2.length()) { //match -> minus to count
 				n--;
 				startIndex++;
 				index = 0;
-			}
-			if (n == 0) {
+			} else if (n == 0) { //match require count
 				System.out.print('Y');
 				return;
-			}
-			if (startIndex + index == string.length()){
+			} else if (startIndex + index == string.length()) {
+				//no match count in whole string
 				System.out.print('N');
 				return;
-			}
-			if (string.charAt(startIndex + index) == str2.charAt(index)) {
+			} else if (string.charAt(startIndex + index) == str2.charAt(index)) {
+				//match one char -> test next char
 				index++;
-			} else {
+			} else { //not match -> test next substring
 				startIndex++;
 				index = 0;
 			}
@@ -72,24 +71,25 @@ class Regex {
 
 	public void containAB()
 	{
+		//all case must contain "a" and "bb"
 		int aIndex = -1, bIndex = -1;
-		for (int i = 0; i < string.length(); i++) {
+		for (int i = 0; i < string.length(); i++) { //find "a" from the front
 			if (string.charAt(i) == 'a') {
 				aIndex = i;
 				break;
 			}
 		}
-		if (aIndex == -1) {
+		if (aIndex == -1) { //"a" must find once
 			System.out.print('N');
 			return;
 		}
-		for (int i = string.length() - 1; i > 0; i--) {
+		for (int i = string.length() - 1; i > 0; i--) { //find "bb" from the end
 			if (string.charAt(i) == 'b' && string.charAt(i - 1) == 'b') {
 				bIndex = i;
 				break;
 			}
 		}
-		if (bIndex > aIndex)
+		if (bIndex > aIndex) //if "bb" is at the right of "a"
 			System.out.print('Y');
 		else
 			System.out.print('N');
