b89f000b2433c8a88450467a2768924d66cee076
Hell HBBD <paulyu602@gmail.com>
2024-03-05 09:56:53 +0800

    use dynamic memory to generate msg

--- java/112PD2/submit.c
+++ java/112PD2/submit.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char *getMsg(char *arg){
+char *getMsg(char *arg)
+{
+	const char *str1 = "scp -P 32024 -r hw";
+	const char *str2 = "/*.java H34111122@140.116.246.208:~/hw";
+	char *msg = malloc(sizeof(char) * (strlen(str1) + strlen(str2) + 2 * strlen(arg) + 1));
+	strcpy(msg, str1);
+	strcat(msg, arg);
+	strcat(msg, str2);
+	strcat(msg, arg);
+	return msg;
 }
 
 int main(int argc, char **argv)
@@ -10,8 +20,9 @@ int main(int argc, char **argv)
 		fputs("no specified directory", stderr);
 		return -1;
 	}
-	char msg[100] = { 0 };
-	sprintf(msg, "scp -P 32024 -r hw%s/*.java H34111122@140.116.246.208:~/hw%s/", argv[1], argv[1]);
+	char *msg = getMsg(argv[1]);
 	system(msg);
+	/* printf("%s\n", msg); */
+	free(msg);
 	return 0;
 }
