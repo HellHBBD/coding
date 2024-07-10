79e9afe26013a68b5024b0badb62ab9a327cdc2a
Hell HBBD <paulyu602@gmail.com>
2024-07-10 14:20:08 +0800

    delete other/

--- other/C/ASCII_code.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	for (int i = 0; i < 128; i++)
-		printf("\'%c\' %d\n", (char)i, i);
-}
--- other/C/printBits.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "printBits.h"
-
-int main()
-{
-	int x = 1234567800;
-	printBits(sizeof(x), &x);
-	return 0;
-}
